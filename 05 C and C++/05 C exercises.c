82c7d64f839e4ed8ad4d7125393eea875d3d59af
lxmora <154564516+lxmora@users.noreply.github.com>
2023-12-21 23:50:07 +0000

    Initial commit

---
+++ 05 C and C++/05 C exercises.c
@@ -0,0 +1,146 @@
+#include <stdio.h>
+
+// README: 
+// Skip down to the exercise functions that are outlined below. 
+// Above each exercise you'll see a set of instructions. 
+
+/* Here are the definitions for each function */
+
+void exercise1();
+void exercise2();
+void exercise3();
+void exercise4();
+void exercise5();
+
+/* This is the main function - where programs in C start */
+/* For the purpose of this file, we'll use it to run your exercise attempts */
+
+int main() {
+
+    printf("Hello, World!\n");
+    printf("Welcome to your exercises in C!\n\n");
+
+    exercise1();
+    //call exercise2() here...
+    //call exercise3() here...
+    //call exercise4() here...
+    //call exericse5() here...
+
+    return 0; 
+
+}
+
+// Exercise 1 
+// In the function below, ask the user to type in their age. 
+// Store this in a variable and then output this to the screen. 
+// In Python, the function is input(), but what is it in C?
+// Also remember to declare your variables with TYPES in C and use semi-colons!  
+//
+// Hint: if you get stuck, look up the 'C format specifiers'
+//
+// Extension: Also ask the user to type in their name, store this in a variable, and then output this to the screen.
+
+void exercise1(){
+
+    printf("\nHello from the exercise 1 function...\n");
+
+    //Python: 
+    // age = int(input("Please enter your age"))
+
+    // Write the solution to exercise 1 here...
+} 
+
+// Exercise 2 
+// Now write a for loop in C that will repeat five times and print the value of the loop counter to the screen.
+//
+// Print the values on the same line: 1,2,3,4,5
+// Then also modify the loop so it prints each value on a new line: 
+// 1
+// 2
+// 3
+// 4
+// 5
+//
+// In Python, you might write the for.. in... loop, but what about C?
+// Remember to call exercise2() from the main function above!
+
+void exercise2(){
+
+    //Python: 
+    //for x in range(1,6)
+    //   print(x)
+
+    //C: (also see the structure of the C for loop written in English below)
+    //for(initialise loop counter variable here; compare value of variable here; increment loop counter)
+    //Write your solution here.
+} 
+
+// Exercise 3 
+// Write a series of conditional statements that will output the lettergrade for a mark between 0 to 100.
+// You may have already written this in Python, so feel free to translate the code into C!
+// 
+// Extension: Create a C function that will also convert lettergrades (e.g. 'A', 'B', 'C') 
+// to University degree classifications (e.g. "1st", "2:1", "2:2"). Can you write a switch statement to do this?
+// 
+
+void exercise3(){
+
+    // Python: 
+    // mark = int(input("Please enter your mark:"))
+    // if mark >= 0 and mark < 40:
+    //    print("Not successful")
+    // elif mark >= 40 and mark < 50:
+    //    print("This is a D")
+    // else:
+    //    print("More to complete...")
+
+    // C:
+    // Write your solution here.
+
+} 
+
+// Exercise 4
+// Now define a C array which is fixed-size and can store 10 integers. 
+// Assign the values 1 to 10, each to an indivdiual elememt of the integer array.
+// Output the contents of the array to the screen.
+// 
+// Hint: in Python you could state the name of the list to print all the values in a row, 
+// but in C, you'll need to loop through each element. 
+// 
+// Extension: Randomly generate the contents of the array with values between 1 and 10. Check this is random each time by printing it to the screen.
+
+
+void exercise4(){
+
+    // Python List: 
+    // l = [1,2,3,4,5,6,7,8,9,10]
+    // for i in l:
+    //    print(i)
+
+    // C array:
+    // Write the solution to exercise 4 here...
+} 
+
+
+// Exercise 5
+// Classes were introduced for C++, along with the other principles of OOP, but C did have a structure (struct).
+// These structures were useful for encapsulating related variables (objects) for an entity, 
+// but the procedures (functions/methods) would be separate (not defined as part of the structure). 
+// You'll see this separation below. 
+//
+// Create a variable of the Student struct defined here and assign values for its name and id 'attributes'
+//
+// Extension 1: Ask the user to enter this data from the keyboard.
+// Extension 2: Now scale this for three students. Store the three students in an array. Then iterate through the array to print their details.
+
+struct Student{
+    char name[30];
+    int id;
+};
+
+void exercise5(){
+
+    // Write the solution to exercise 5 here...
+    
+} 
+
---
+++ 09 PyGame (Python)/Invaders/Main.py
@@ -0,0 +1,99 @@
+''' Space Invaders attempt by ND'''
+
+#specific for this repo - set the file dir
+import os
+#print("The Current working directory is: {0}".format(os.getcwd()))
+os.chdir(os.getcwd() + "/09 PyGame (Python)/Invaders/")
+print("The Current working directory is: {0}".format(os.getcwd()))
+
+
+# Import and initialize the pygame library
+import pygame
+from Player import Player
+from Invader import Invader
+
+pygame.init()
+
+# Set 'constants' for max X and Y size for the drawing window
+SCREEN_X = 500
+SCREEN_Y = 650
+screen = pygame.display.set_mode([SCREEN_X, SCREEN_Y])
+
+# 1.0 instantiate Player class
+player = Player((SCREEN_X/2)-(35/2), (SCREEN_Y - 100), pygame.image.load("img/defender.png"), 35, 30)
+
+# 1.1 instantiate Invader class
+invader = Invader(50, 100, pygame.image.load("img/invader1.png"), 30, 30 )
+
+invaders_x_left = 50
+invaders_y_top = 100
+
+#1.2 invaders = [0,0,0,0,0,0,0,0,0,0,0,0]
+invaders = [0] * 11 #short hand way to initialise all 11 mem spaces for ONE ROW
+for i in range(0,11): # 0 to 10 - create one row of 11 cols
+    invaders[i] = Invader(invaders_x_left + (i * 40), 100, pygame.image.load("img/invader1.png"), 30, 30) #instantiate one row 
+
+#1.3 2D table of invaders 
+rows = 5
+cols = 11
+           # [[0] * col for _ in range(row)]
+invaders2D = [[0] * cols for i in range(rows)] #2D array using the Python list comprehension
+
+current_row = invaders_y_top
+#print(invaders2D)
+for i in range(rows): # 5 rows
+    for j in range(cols): # 0 to 10 - create one row of 11 cols
+        #invaders2D[i][j] = ((invaders_x_left + (j * 30) + 20), current_row ) #for testing the COORDINATES
+        invaders2D[i][j] = Invader((invaders_x_left + (j * 30) + 30), current_row, pygame.image.load("img/invader1.png"), 30, 30) #instantiate one row 
+    current_row = invaders_y_top + (50 * (i+1)) # distance between rows
+
+#print(invaders2D) # for testing the COORDINATES
+
+# Fill the background with black - as it is Space Invaders
+screen.fill((0, 0, 0))
+
+
+# Run until the user asks to quit
+running = True
+while running:
+
+    # Did the user click the window close button?
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    #1.0 draw the png on the screen!
+    #screen.blit(player.img, (player.x, player.y))
+    
+    #1.1 draw one of the invaders
+    #screen.blit(invader.img, (invader.x, invader.y))
+    
+    #1.2 draw a row of invaders
+    #i = 0
+    #for x in invaders:
+    #    screen.blit(invaders[i].img, (invaders[i].x, invaders[i].y))
+    #    i += 1
+    
+    #1.3 draw 2d table of invaders
+    for i in range(rows):
+        for j in range(cols):
+            screen.blit(invaders2D[i][j].img, (invaders2D[i][j].x, invaders2D[i][j].y))
+
+    #1.0 detect keypress
+    if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_LEFT:
+            #screen.blit(player.img, (player.x, player.y)) 
+            #player.x -= 0.05
+            player.move_left() #player.x -= 0.05     # rellocated to a method!
+            
+        if event.key == pygame.K_RIGHT:
+            #player.x += 0.05  
+            player.move_right() 
+    
+    screen.blit(player.img, (player.x, player.y))       
+    
+    # Flip the display
+    pygame.display.flip()
+
+# End
+pygame.quit()
